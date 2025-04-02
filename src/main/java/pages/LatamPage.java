35a5dc72fa08bfa5f0bb70dd77494572f860f6b8
FZeaa <162461334+FZeaa@users.noreply.github.com>
2025-04-02 17:17:26 -0500

    Update LatamPage.java
    add 3 new xpaths

--- src/main/java/pages/LatamPage.java
+++ src/main/java/pages/LatamPage.java
@@ -23,9 +23,13 @@ public class LatamPage {
     private By latamPackagesImg = By.xpath("//img[@src='/content/dam/latamxp/sites/travel/mvp-subhome/banners-promocionales/co/campañas/2025/san-valentin/Subhome LT Banner Oferta 864x500 D! Desde (2).png']");
     private By originTextField = By.xpath("//input[@data-testid='fsb-origin--text-field']");
     private By departureTextField = By.xpath("//div[@data-testid='fsb-departure--text-field__wrapper']");
+    private  By returnTextField = By.xpath("//div[@data-testid='fsb-return--text-field__wrapper']");
     private By searchFlightsBtn = By.xpath("//button[@data-testid='fsb-search-flights--button']");
     private By airportOptionLabel = By.xpath("//span[contains(@data-testid,'airport--autocomplete')]");
     private By destinationErrorMessageLabel = By.xpath("//div[@data-testid='fsb-destination--text-field__helper-text']");
+    private By oneWayButton = By.xpath("//div[@data-testid='fsb-one-way']");
+    private By tabFlightGrid = By.xpath("//div[@class='InputsGrid-sc-akbnfi-0 ktdkvt']");
+    private By closeLoginIncentiveBtn = By.xpath("//button[@data-testid='button-close-login-incentive']");
 
     /**
      * @return element for offers and destination button
@@ -63,6 +67,13 @@ public class LatamPage {
     }
 
     /**
+     * @return element for Return text field
+     */
+    public WebElement getReturnTextFieldEle(){
+        return driver.findElement(returnTextField);
+    }
+
+    /**
      * @return element for search flights button
      */
     public WebElement getSearchFlightsBtnEle(){
@@ -70,16 +81,37 @@ public class LatamPage {
     }
 
     /**
-     * @return element airport Options Label
+     * @return element for airport Options Label
      */
     public WebElement getAirportOptionLabelEle(){
         return driver.findElement(airportOptionLabel);
     }
 
     /**
-     * @return element destination error message label
+     * @return element for destination error message label
      */
     public WebElement getDestinationErrorMessageLabelEle(){
         return driver.findElement(destinationErrorMessageLabel);
+    }
+
+    /**
+     * @return element for one way button
+     */
+    public WebElement getOneWayBtnEle(){
+        return driver.findElement(oneWayButton);
+    }
+
+    /**
+     * @return element for tab flight grid
+     */
+    public WebElement getTabFlightGridEle(){
+        return driver.findElement(tabFlightGrid);
+    }
+
+    /**
+     * @return element for close login incentive button 
+     */
+    public WebElement getCloseLoginIncentiveBtnEle(){
+        return driver.findElement(closeLoginIncentiveBtn);
     }
 }
