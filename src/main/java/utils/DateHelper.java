266e84ad7bbd63c365f91c171ded2445d5a7e527
FZeaa <162461334+FZeaa@users.noreply.github.com>
2025-04-02 16:36:08 -0500

    Create DateHelper.java
    Add new field, to add a Date method, this with the aim to reused it in the furute

---
+++ src/main/java/utils/DateHelper.java
@@ -0,0 +1,26 @@
+package utils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class DateHelper {
+
+    /**
+     * @return
+     */
+    public String getCurrentDate() {
+        return LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String generateFutureDate() {
+        LocalDate today = LocalDate.now();
+        long randomDays = ThreadLocalRandom.current().nextLong(1, 29 + 1);
+        LocalDate futureDate = today.plusDays(randomDays);
+        return futureDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
+}
