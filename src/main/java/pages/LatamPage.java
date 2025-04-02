afcff671cdc65d103448a22c06c3b9a712dd8a90
FZeaa <162461334+FZeaa@users.noreply.github.com>
2025-04-02 09:23:29 -0500

    Update LatamPage.java
    add a findElement method, a xpath and method to return it as a element

--- src/main/java/pages/LatamPage.java
+++ src/main/java/pages/LatamPage.java
@@ -1,16 +1,44 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class LatamPage {
 
     private WebDriver driver;
-
-    private By homeTitle = By.id("homeTitle");
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(LatamPage.class);
     public LatamPage(WebDriver driver) {
         this.driver = driver;
+    }
+
+
+    private By offersAndDestinationsBtn = By.xpath("//button[@title='Ofertas y destinos']");
+
+
+    /**
+     *
+     */
+    private WebElement findElementBy(By by){
+        LOGGER.info("[FindElementBy]By to find: {}", by);
+        try {
+            return driver.findElement(by);
+        } catch (NoSuchElementException nse){
+            LOGGER.debug("By: {} is not displayed/available. Returning Null", by);
+            return null;
+        }
+    }
+
+
+    /**
+     *
+     */
+    public WebElement getOffersAndDestinationsBtnEle(){
+        return findElementBy(offersAndDestinationsBtn);
     }
 
 }
