24f85b7197806f2a8d027f34564d9b2c10c8fccb
FZeaa <162461334+FZeaa@users.noreply.github.com>
2025-04-02 16:41:12 -0500

    Update LatamPage.java
    Add new xpaths, and deleted the find element method to use the driver.findelement method

--- src/main/java/pages/LatamPage.java
+++ src/main/java/pages/LatamPage.java
@@ -1,7 +1,6 @@
 package pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
@@ -22,40 +21,65 @@ public class LatamPage {
     private By offersAndDestinationsBtn = By.xpath("//button[@title='Ofertas y destinos']");
     private By FlightOffersBtn = By.xpath("//span[@data-testid='header__navbar__lnk-flight-offers--menuitem__label-content'][contains(.,'Ofertas de vuelos')]");
     private By latamPackagesImg = By.xpath("//img[@src='/content/dam/latamxp/sites/travel/mvp-subhome/banners-promocionales/co/campañas/2025/san-valentin/Subhome LT Banner Oferta 864x500 D! Desde (2).png']");
-
-    /**
-     * Method to find an element using the driver using the input by
-     * @param by
-     * @return element for specific
-     */
-    private WebElement findElementBy(By by){
-        LOGGER.info("[FindElementBy]By to find: {}", by);
-        try {
-            return driver.findElement(by);
-        } catch (NoSuchElementException nse){
-            LOGGER.debug("By: {} is not displayed/available. Returning Null", by);
-            return null;
-        }
-    }
+    private By originTextField = By.xpath("//input[@data-testid='fsb-origin--text-field']");
+    private By departureTextField = By.xpath("//div[@data-testid='fsb-departure--text-field__wrapper']");
+    private By searchFlightsBtn = By.xpath("//button[@data-testid='fsb-search-flights--button']");
+    private By airportOptionLabel = By.xpath("//span[contains(@data-testid,'airport--autocomplete')]");
+    private By destinationErrorMessageLabel = By.xpath("//div[@data-testid='fsb-destination--text-field__helper-text']");
 
     /**
      * @return element for offers and destination button
      */
     public WebElement getOffersAndDestinationsBtnEle(){
-        return findElementBy(offersAndDestinationsBtn);
+        return driver.findElement(offersAndDestinationsBtn);
     }
 
     /**
      * @return element for flight offers button
      */
     public  WebElement getFlightOffersBtnEle(){
-        return findElementBy(FlightOffersBtn);
+        return driver.findElement(FlightOffersBtn);
     }
 
     /**
      * @return element for latam packages imagen
      */
     public WebElement getLatamPackagesImgEle(){
-        return findElementBy(latamPackagesImg);
+        return driver.findElement(latamPackagesImg);
+    }
+
+    /**
+     * @return element for origin text field
+     */
+    public  WebElement getOriginLabelEle(){
+        return driver.findElement(originTextField);
+    }
+
+    /**
+     * @return element for Departure text field
+     */
+    public WebElement getDepartureTextFieldEle(){
+        return driver.findElement(departureTextField);
+    }
+
+    /**
+     * @return element for search flights button
+     */
+    public WebElement getSearchFlightsBtnEle(){
+        return driver.findElement(searchFlightsBtn);
+    }
+
+    /**
+     * @return element airport Options Label
+     */
+    public WebElement getAirportOptionLabelEle(){
+        return driver.findElement(airportOptionLabel);
+    }
+
+    /**
+     * @return element destination error message label
+     */
+    public WebElement getDestinationErrorMessageLabelEle(){
+        return driver.findElement(destinationErrorMessageLabel);
     }
 }
