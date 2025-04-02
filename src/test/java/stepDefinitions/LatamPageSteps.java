3be758daea0835cca78b453224f206d7b447d8ac
FZeaa <162461334+FZeaa@users.noreply.github.com>
2025-04-02 16:40:37 -0500

    Update LatamPageSteps.java
    add a new button in the click method, also added 3 methods to add text in the destination text field, select random date, and method to verify if the error message is show

--- src/test/java/stepDefinitions/LatamPageSteps.java
+++ src/test/java/stepDefinitions/LatamPageSteps.java
@@ -3,23 +3,28 @@ package stepDefinitions;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.testng.Assert;
 import pages.LatamPage;
+import utils.DateHelper;
 import config.DriverConfig;
 import utils.AssertUtil;
 import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebElement;
 
 public class LatamPageSteps {
 
     private WebDriver driver;
     private LatamPage latamPage;
+    private DateHelper dateHelper;
     private static final Logger LOGGER = LoggerFactory.getLogger(LatamPageSteps.class);
-    public LatamPageSteps(DriverConfig driverConfig, LatamPage latamPage) {
+    public LatamPageSteps(DriverConfig driverConfig, LatamPage latamPage, DateHelper dateHelper) {
         this.driver = driverConfig.getDriver();
         this.latamPage = latamPage;
+        this.dateHelper = dateHelper;
     }
 
     /**
@@ -35,7 +40,7 @@ public class LatamPageSteps {
      * Method for clicking on specific buttons
      * @param buttonName name of the button
      */
-    @And("I Click in the {string} button")
+    @And("I click in the {string} button")
     public void clickInTheButton(String buttonName) {
       LOGGER.debug("Clicking in the {} button", buttonName);
      switch (buttonName){
@@ -44,6 +49,9 @@ public class LatamPageSteps {
              break;
          case "Ofertas de vuelos":
              latamPage.getFlightOffersBtnEle().click();
+             break;
+         case "Buscar Vuelos":
+             latamPage.getSearchFlightsBtnEle().click();
              break;
          default:
              throw new IllegalArgumentException(String.format("Invalid input:", buttonName));
@@ -69,4 +77,42 @@ public class LatamPageSteps {
         Assert.assertTrue(latamPage.getLatamPackagesImgEle().isDisplayed());
     }
 
+    /**
+     * Method to add a flight in the from text field
+     * @param origin city or airport of origin
+     */
+    @When("I search for a flight from {string}")
+    public void iSearchForAFlightFromTo(String origin) {
+        LOGGER.debug("Searching for a flight from " + origin );
+        latamPage.getOriginLabelEle().sendKeys(origin);
+        latamPage.getAirportOptionLabelEle().click();
+    }
+
+    /**
+     * Method to select the actual date and random date with 30 days of difference
+     */
+    @And("I select a random date")
+    public void iSearchInTheDateFromTo() {
+        latamPage.getDepartureTextFieldEle().click();
+        LOGGER.debug("Selecting the Actual date...");
+        String currentDate = dateHelper.getCurrentDate();
+        String currentDateXPath = String.format("//span[@data-testid='date-%s']", currentDate);
+        WebElement currentDateElement = driver.findElement(By.xpath(currentDateXPath));
+        currentDateElement .click();
+        latamPage.getDepartureTextFieldEle().click();
+        LOGGER.debug("Selecting a random future date...");
+        String futureDate = dateHelper.generateFutureDate();
+        String futureDateXPath = String.format("//span[@data-testid='date-%s']", futureDate);
+        WebElement futureDateElement = driver.findElement(By.xpath(futureDateXPath));
+        futureDateElement.click();
+    }
+
+    /**
+     * Method to verify if the error message in the destination text field is displayed 
+     */
+    @Then("I should be able to see the error message in the Destination text field")
+    public void iShouldBeAbleToSeeTheErrorMessageInTheDestinationTextField() {
+        LOGGER.debug("Checking if the destination error message is displayed...");
+        Assert.assertTrue(latamPage.getDestinationErrorMessageLabelEle().isDisplayed());
+    }
 }
